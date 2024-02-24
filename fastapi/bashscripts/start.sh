8bf383153635c13d7755fb13ba2a88b35e03e429
ai_mamontov <ai.mamontov@rusagro.tech>
2024-02-24 17:05:06 +0300

    changed ports, because 8000 is busy in most pc

--- fastapi/bashscripts/start.sh
+++ fastapi/bashscripts/start.sh
@@ -5,7 +5,7 @@ ENV_FILE=".env"
 if [ ! -f "$ENV_FILE" ]; then
     echo "Создаем .env файл..."
     
-    echo "FASTAPI_PORT=8000" >> "$ENV_FILE"
+    echo "FASTAPI_PORT=6981" >> "$ENV_FILE"
     echo "JWT_SECRET_KEY=d_-AbS6mp1yOUPH2x7Xbf2fzTNU-9vB6jqiK-1LidrE" >> "$ENV_FILE"
     echo "JWT_REFRESH_SECRET_KEY=RJy8QV0LjSsWANcfO9kC8XJZL4bfwtFwj3L1JW4ZDwo" >> "$ENV_FILE"
 
@@ -13,7 +13,7 @@ if [ ! -f "$ENV_FILE" ]; then
     echo "POSTGRES_USER=postgres_user" >> "$ENV_FILE"
     echo "POSTGRES_PASSWORD=postgres_password" >> "$ENV_FILE"
 
-    echo "PGADMIN_PORT=8001" >> "$ENV_FILE"
+    echo "PGADMIN_PORT=6982" >> "$ENV_FILE"
     echo "PGADMIN_DEFAULT_EMAIL=admin@admin.ru" >> "$ENV_FILE"
     echo "PGADMIN_DEFAULT_PASSWORD=password" >> "$ENV_FILE"
 
