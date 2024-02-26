b1ba95f2be8b401e4ea1666b1e8b544cad0e45b2
danst <danstolyarov79@gmail.com>
2024-02-26 10:55:16 +0300

    Created Navbar Component

--- dormitory.front/dormitory.front.Client/Program.cs
+++ dormitory.front/dormitory.front.Client/Program.cs
@@ -2,4 +2,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost")
+    });
+
 await builder.Build().RunAsync();
--- dormitory.front/dormitory.front/Program.cs
+++ dormitory.front/dormitory.front/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
