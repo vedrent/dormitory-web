5acd06837e0e241cb361a82f963b80271a0f3cf3
danst <danstolyarov79@gmail.com>
2024-03-02 18:59:02 +0300

    added slide menu with options

---
+++ dormitory.front/dormitory.front.Client/model/Option.cs
@@ -0,0 +1,15 @@
+﻿namespace dormitory.front.Client.model
+{
+    public class Option
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+
+        public Option(string title, string description, string iconUrl) {
+            Title = title;
+            Description = description;
+            IconUrl = iconUrl;
+        }
+    }
+}
