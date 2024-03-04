016c516fbd25b432f8acdaa4dd35393bf8f38697
danst <danstolyarov79@gmail.com>
2024-03-04 11:03:43 +0300

    added News CollectionView

--- dormitory.front/dormitory.front.Client/model/NewsItemModel.cs
+++ dormitory.front/dormitory.front.Client/model/NewsItemModel.cs
@@ -6,5 +6,10 @@ public class NewsItemModel
     public string Content { get; set; }
     public string Source { get; set; }
 
-    
+    public NewsItemModel(string title, string content, string source)
+    {
+        Title = title;
+        Content = content;
+        Source = source;
+    }
 }
